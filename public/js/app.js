61b2097506850f8f77ce22efa6d15ea748e0778e
Pramod Shenoy <pramodshenoy@Pramod-Macbook-Pro.local>
2020-09-23 07:52:59 -0400

    space  removed

--- public/js/app.js
+++ public/js/app.js
@@ -19,7 +19,6 @@ weatherForm.addEventListener('submit', (e)=>{
             messageOne.textContent=data.location;
             messageTwo.textContent=data.forecast.Conditions;
         }
-        
     })
 })
 })
